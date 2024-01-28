8a6f8a01d10a089d1833f9e3e8ab0f47b8b948a4
ElbekLatifov <latifovelbek02@gmail.com>
2024-01-28 18:45:37 +0500

    end2

--- TaskBlazor/Models/TaskModel.cs
+++ TaskBlazor/Models/TaskModel.cs
@@ -1,12 +1,16 @@
-﻿using TaskBlazor.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskBlazor.Entities;
 
 namespace TaskBlazor.Models
 {
     public class TaskModel
     {
+        [Required(ErrorMessage = "Enter title")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Enter description")]
         public string Description { get; set; }
         public State State { get; set; }
+        [Required(ErrorMessage = "Enter date")]
         public DateTime Deadline { get; set; }
     }
 }
